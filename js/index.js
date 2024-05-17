55d5af3b2653a46e14a548e319e8d60c0d7daae5
jasur aminjonov <jasur9414@gmail.com>
2024-05-17 19:43:06 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//let surname = "Aminjonov";
+
+//const pr = prompt("Ismingizni kiriting");
+//let isDelete = confirm("Rosdan shu malumotni ochmoqchimisiz");//
+
+//document.write(surname);
+
+// 1-misol
+// let L = +prompt("Uzunlikni sm larda kiriting");
+// let m = L / 100;
+// console.log(m);
+
+// 2-misol
+let k = +prompt("kilogramdi t larda kiriting");
+let t = k / 1000;
+console.log(t);
